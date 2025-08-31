3ce9784f0093dd495a3ef84b3165086414de2d20
mxmf <2993747433@qq.com>
2025-08-31 13:41:45 +0800

    chore(release): bump version to 0.1.2

--- nix/flake.nix
+++ nix/flake.nix
@@ -19,7 +19,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "spinmc";
-          version = "0.1.1";
+          version = "0.1.2";
 
           src = ./..;
 
